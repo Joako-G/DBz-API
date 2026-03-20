7fe3df19129008f56bccdf998fc0ab0abd29e052
Joako-G <gallardojoako@gmail.com>
2026-03-20 14:51:51 -0300

    Agrego funcionalidad para inhabilitar boton de favorito si no hay usuario conectado

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -26,7 +26,7 @@ export function Character({ character }: prop) {
 
             <div className={styles.btnActions}>
                 <button> Ver mas </button>
-                <button className={styles.favorite} disabled={!user} onClick={() => toggleFavorite(character.id)}>
+                <button className={styles.favorite} disabled={!user} style={{ pointerEvents: !user ? 'none' : 'auto' }} onClick={() => toggleFavorite(character.id)}>
                     <svg className={styles.heart} style={{ fill: isFavorite(character.id) ? 'red' : 'white' }} width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
                         strokeLinejoin="round">
                         <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
