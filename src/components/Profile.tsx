c9f9810d98e554e86bbd2badd2445e1688eb742a
Joako-G <gallardojoako@gmail.com>
2026-03-21 14:57:54 -0300

    Agrego componente para mostrar la lista de favoritos mediante cards

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,36 @@
 import type { UserInside } from "../interfaces/user.type"
 import styles from './Profile.module.css'
 import userImg from '../assets/user.png'
+import type { CharacterId } from "../interfaces/character.types"
+import { Character } from "./Character"
 
 interface ProfileProps {
-    user: UserInside
+    user: UserInside;
+    favorites: CharacterId[]
 }
 
-export function Profile({ user }: ProfileProps) {
+// function useCharacters(favorites: number[]) {
+//     let characters = []
+
+//     useEffect(() => {
+//         async function getCharacterId(id: number) {
+//             try {
+//                 const response = await fetch(`https://dragonball-api.com/api/characters/${id}`)
+//                 const json = await response.json()
+//                 return json
+//             } catch (error) {
+//                 throw new Error('Ha ocurrido un problema inesperad!!!')
+//             }
+//         }
+
+
+
+//     }, [])
+
+// }
+
+export function Profile({ user, favorites }: ProfileProps) {
+    console.log('Favorites Profile: ', favorites)
     return (
         <div className={styles.container}>
             <section className={styles.userSection}>
@@ -18,8 +42,12 @@ export function Profile({ user }: ProfileProps) {
             </section>
 
             <section className={styles.userFav}>
-                <div>
-                    Lista de favoritos: []
+                <div className={styles.cards}>
+                    {
+                        favorites.map((fav) => (
+                            <Character character={fav} key={fav.id} />
+                        ))
+                    }
                 </div>
             </section>
         </div>
