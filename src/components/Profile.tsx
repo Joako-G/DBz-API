79b5034a06650b442a57e264f85e8a8dc2bd5778
Joako-G <gallardojoako@gmail.com>
2026-03-22 11:15:44 -0300

    Modifico props para trabajar correctamente con los datos de la API

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,36 +1,15 @@
 import type { UserInside } from "../interfaces/user.type"
 import styles from './Profile.module.css'
 import userImg from '../assets/user.png'
-import type { CharacterId } from "../interfaces/character.types"
+import type { character } from "../interfaces/character.types"
 import { Character } from "./Character"
 
 interface ProfileProps {
     user: UserInside;
-    favorites: CharacterId[]
+    favorites: character[]
 }
 
-// function useCharacters(favorites: number[]) {
-//     let characters = []
-
-//     useEffect(() => {
-//         async function getCharacterId(id: number) {
-//             try {
-//                 const response = await fetch(`https://dragonball-api.com/api/characters/${id}`)
-//                 const json = await response.json()
-//                 return json
-//             } catch (error) {
-//                 throw new Error('Ha ocurrido un problema inesperad!!!')
-//             }
-//         }
-
-
-
-//     }, [])
-
-// }
-
 export function Profile({ user, favorites }: ProfileProps) {
-    console.log('Favorites Profile: ', favorites)
     return (
         <div className={styles.container}>
             <section className={styles.userSection}>
