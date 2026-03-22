5e93ef95744bee34a7ebba37592168ba6c075b2c
Joako-G <gallardojoako@gmail.com>
2026-03-22 11:30:10 -0300

    Agrego layout para mostrar en pantalla

---
+++ src/pages/CharacterPage.tsx
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom";
+import { CharacterInfo } from "../components/CharacterInfo";
+import { useEffect, useState } from "react";
+import type { CharacterId } from "../interfaces/character.types";
+import styles from './CharacterPage.module.css'
+
+export function CharacterPage() {
+    const [character, setCharacter] = useState<CharacterId>()
+    const params = useParams()
+
+    useEffect(() => {
+        async function getCharacterId(id: number) {
+            try {
+                const response = await fetch(`https://dragonball-api.com/api/characters/${id}`)
+                const json = response.json()
+                return json
+            } catch (error) {
+                throw new Error('Ha ocurrido un problema inesperad!!!')
+            }
+        }
+
+        getCharacterId(Number(params.id))
+            .then(data => setCharacter(data))
+    }, [])
+
+    return (
+        <div className={styles.container}>
+            <CharacterInfo character={character!} />
+        </div>
+    )
+}
