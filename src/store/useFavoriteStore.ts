b42445d4376d4c67d08377978a241f235189d186
Joako-G <gallardojoako@gmail.com>
2026-03-25 09:39:36 -0300

    feat: Modifico store para recibir los datos de la API y actualizo la store de user

--- src/store/useFavoriteStore.ts
+++ src/store/useFavoriteStore.ts
@@ -1,9 +1,12 @@
 import { create } from "zustand";
+import { useAuthStore } from "./useAuthStore";
+import { updateFavorite } from "../services/serviceChar";
+import type { UserInside } from "../interfaces/user.type";
 
 interface FavoriteState {
     favorites: number[];
-    addFavorite: (charId: number) => void;
-    removeFavorite: (charId: number) => void;
+    addFavorite: (charId: number) => Promise<void>;
+    removeFavorite: (charId: number) => Promise<void>;
     isFavorite: (charId: number) => boolean;
     toggleFavorite: (charId: number) => void;
     countFavorite: () => void;
@@ -12,20 +15,52 @@ interface FavoriteState {
 
 export const useFavoriteStore = create<FavoriteState>((set, get) => ({
     favorites: [],
-    addFavorite: (charId) => {
+    addFavorite: async (charId) => {
         set((state) => (
             {
                 favorites: state.favorites.includes(charId) ? state.favorites : [...state.favorites, charId]
             }
         ))
+
+        const { user } = useAuthStore.getState()
+        const favorites = get().favorites
+
+        if (user?.id) {
+            const data: UserInside = await updateFavorite(user.id, favorites)
+
+            useAuthStore.setState({
+                user: {
+                    ...user,
+                    favorites: data.favorites
+                }
+            })
+
+        }
+
     },
 
-    removeFavorite: (charId) => {
+    removeFavorite: async (charId) => {
         set((state) => (
             {
                 favorites: state.favorites.filter((id) => charId !== id)
             }
         ))
+
+        const { user } = useAuthStore.getState()
+        const favorites = get().favorites
+
+        if (user?.id) {
+            const data: UserInside = await updateFavorite(user.id, favorites)
+
+            useAuthStore.setState({
+                user: {
+                    ...user,
+                    favorites: data.favorites
+                }
+            })
+
+        }
+
     },
 
     isFavorite: (charId) => {
@@ -44,5 +79,7 @@ export const useFavoriteStore = create<FavoriteState>((set, get) => ({
 
     clearFavorite: () => {
         set({ favorites: [] })
-    }
+    },
+
+
 }))
