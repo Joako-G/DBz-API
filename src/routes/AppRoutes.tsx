e6fe3a10666f8940767e77c8e16c189541b6b446
Joako-G <gallardojoako@gmail.com>
2026-03-28 12:57:49 -0300

    Elimino el componente Suspense

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,7 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-// import { LoginPage } from "../pages/LoginPage";
 import { ProtectedRoute } from "../components/ProtectedRoute";
 import { HomePage } from "../pages/HomePage";
-// import { ListCharacters } from "../pages/ListCharacters";
-// import { ProfilePage } from "../pages/ProfilePage";
-// import { CharacterPage } from "../pages/CharacterPage";
-// import { RegisterPage } from "../pages/RegisterPage";
 
 const PageLogin = lazy(() => import('../pages/LoginPage'))
 const PageCharacters = lazy(() => import('../pages/ListCharacters'))
@@ -16,50 +11,47 @@ const PageRegister = lazy(() => import('../pages/RegisterPage'))
 
 export function AppRoutes() {
     return (
-        <Suspense fallback={<div style={{ margin: '0 auto', padding: '0 1rem' }}> Cargando </div>}>
+        <Routes>
+            <Route path='/login' element={<PageLogin />} />
 
-            <Routes>
-                <Route path='/login' element={<PageLogin />} />
+            <Route
+                path='/'
+                element={
+                    <HomePage />
+                }
+            />
 
-                <Route
-                    path='/'
-                    element={
-                        <HomePage />
-                    }
-                />
+            <Route
+                path='/characters'
+                element={
+                    <PageCharacters />
+                }
+            />
 
-                <Route
-                    path='/characters'
-                    element={
-                        <PageCharacters />
-                    }
-                />
+            <Route
+                path='/profile'
+                element={
+                    <ProtectedRoute>
+                        <PageProfile />
+                    </ProtectedRoute>
+                }
+            />
 
-                <Route
-                    path='/profile'
-                    element={
-                        <ProtectedRoute>
-                            <PageProfile />
-                        </ProtectedRoute>
-                    }
-                />
+            <Route
+                path='/characters-detail/:id'
+                element={
+                    <PageCharacter />
+                }
+            />
 
-                <Route
-                    path='/characters-detail/:id'
-                    element={
-                        <PageCharacter />
-                    }
-                />
+            <Route
+                path='/register'
+                element={
+                    <PageRegister />
+                }
+            />
 
-                <Route
-                    path='/register'
-                    element={
-                        <PageRegister />
-                    }
-                />
-
-                {/* <Route path="*" element={<NotFoundPage} /> */}
-            </Routes>
-        </Suspense>
+            {/* <Route path="*" element={<NotFoundPage} /> */}
+        </Routes>
     )
 }
